055186b757dc389f79f4c564ace19f2d5728013f
Teguh <teguh.wicaksono@mmi-pt.com>
2022-04-22 04:41:46 +0700

    up

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const { HomeRouter } = require("./router/index");
 // enable cors
 // app.use(cors);
 
-app.use("/sample-app", HomeRouter);
+// app.use("/sample-app", HomeRouter);
+app.use("/", (req, res) => {
+    res.send("SAMPLE APP")
+} );
 
 app.listen(PORT, () => console.log("SERVER UP AND RUNNING ON PORT " + PORT));
